002debb1c7665c9281a987d5109781267f4ac730
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-04-02 01:50:25 +0500

    In Proccess

---
+++ JavaScriptFirstLesson/JSCRUDProject/crud.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const addCardBtn = document.querySelector("#addCardBtn");
+	const updateCardBtn = document.querySelector("#updateCardBtn");
+	const cardContainer = document.querySelector("#cardContainer");
+	const titleInput = document.querySelector("#titleInput");
+	const contentInput = document.querySelector("#contentInput");
+
+	let cardCount = 0;
+	let currentCard = null;
+
+	console.log("salom");
+
+	addCardBtn.addEventListener("click", function () {
+		// document.querySelector("#addCardBtn").style.display = "block";
+		createCard();
+		// document.querySelector("#updateCardBtn").style.display = "none";
+		titleInput.value = "";
+		contentInput.value = "";
+		currentCard = null;
+		cardCount++;
+	});
+
+	updateCardBtn.addEventListener("click", function () {
+		if (currentCard) {
+			updateCard(currentCard);
+		}
+	});
+
+	function createCard() {
+		cardCount++;
+		const card = document.createElement("div");
+		card.classList.add("card");
+		card.dataset.id = cardCount;
+
+		const cardPhoto = document.createElement("img");
+		cardPhoto.src = "assets/images/PersonalImage.jpg";
+		cardPhoto.classList.add("card-img-top");
+
+		const cardBody = document.createElement("div");
+		cardBody.classList.add("card-body");
+
+		const cardTitle = document.createElement("h5");
+		cardTitle.classList.add("card-title");
+		cardTitle.textContent = titleInput.value || "Title";
+
+		const cardText = document.createElement("p");
+		cardText.classList.add("card-text");
+		cardText.textContent = contentInput.value || "Content";
+
+		const deleteBtn = document.createElement("button");
+		deleteBtn.classList.add("btn", "btn-danger", "mx-2");
+		deleteBtn.textContent = "Delete";
+		deleteBtn.addEventListener("click", function () {
+			card.remove();
+		});
+
+		const updateBtn = document.createElement("button");
+		updateBtn.classList.add("btn", "btn-success", "mx-2");
+		updateBtn.textContent = "Update";
+		updateBtn.setAttribute("data-bs-toggle", "modal");
+		updateBtn.setAttribute("data-bs-target", "#exampleModal");
+
+		updateBtn.addEventListener("click", function () {
+			titleInput.value = cardTitle.textContent;
+			contentInput.value = cardText.textContent;
+			currentCard = card;
+		});
+
+		cardBody.appendChild(cardTitle);
+		cardBody.appendChild(cardText);
+		cardBody.appendChild(updateBtn);
+		cardBody.appendChild(deleteBtn);
+		card.appendChild(cardPhoto);
+		card.appendChild(cardBody);
+
+		if (cardContainer.firstChild) {
+			cardContainer.insertBefore(card, cardContainer.firstChild);
+		} else {
+			cardContainer.appendChild(card);
+		}
+
+		document.querySelector(".btn-close").click();
+	}
+
+	function updateCard(card) {
+		const cardTitle = card.querySelector(".card-title");
+		const cardText = card.querySelector(".card-text");
+		// console.log(document.querySelector("#updateCardBtn").style);
+		// document.querySelector("#addCardBtn").style.display = "block";
+		cardTitle.textContent = titleInput.value || "Title";
+		cardText.textContent = contentInput.value || "Content";
+	}
+});
